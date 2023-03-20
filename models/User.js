7ab446caae598d627ce8004de9324297c04ff46e
Cesar Rodelin Junior <hadajunior5@gmail.com>
2023-03-20 10:19:36 -0400

    Mongoose hooks to hashing the password with bcrypt package

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 //create the schema for an user
 const userSchema = new mongoose.Schema({
@@ -16,6 +17,13 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please enter a password'],
         minlength: [8, 'Minimum password length is 8 characters']
     }
+});
+
+//fire a function before saved the doc to db
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 //use the schema inside a model
