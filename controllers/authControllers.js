25c144c39e82c2b8fd1b06271a52d22a48d38afb
Cesar Rodelin Junior <hadajunior5@gmail.com>
2023-03-20 09:23:50 -0400

    Handle errors validation (Mongoose Validation)

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,23 @@
 const User = require('../models/User');
 
+const handleError = (err) => {
+    let errors = { email: '', password: '' }
+
+    //duplicate key errors
+    if(err.code === 11000){
+        errors.email = 'That email is already registered';
+        return errors;
+    }
+    //validation errors
+    if(err.message.includes('User validation failed')){ 
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -11,8 +29,8 @@ module.exports.signup_post = async (req, res) => {
         const user = await User.create({ email, password });
         res.status(201).json(user);
     } catch (error) {
-        console.log(error);
-        res.status(400).json('error, user not created');
+        const errors = handleError(error);
+        res.status(400).json({ errors });
     }
 }
 
